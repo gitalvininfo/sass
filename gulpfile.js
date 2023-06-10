ad0c0b8ea73e8f3f26179599d710fd9b5a542e03
Alvin Yanson <yanson.alvin.info@gmail.com>
2023-06-10 14:11:59 +0800

    done

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-    return src('*.scss')
+    return src('shinobi/**/*.scss')
         .pipe(sass())
         .pipe(dest('css'))
 }
 
 
 function watchTask() {
-    watch(['*.scss'], buildStyles);
+    watch(['shinobi/**/*.scss'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
